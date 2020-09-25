4e48e653b26f4a58d7b8a466c7f94f883ac1443e
guinessjulie <guiness@hanafos.com>
2020-09-25 12:32:43 +0900

    daily app for 2days make css card

--- quiz/script.js
+++ quiz/script.js
@@ -1,11 +1,19 @@
 const quizData=[
     {
-        quiz : 'How old is Florin?',
-        a : "10",
-        b : "17",
-        c: '26',
-        d: '110',
-        correct: 'c',
+        quiz : 'How to create tensor from python list a = [[1,2,3],[4,5,6]] on pyTorch? ',
+        a : "torch(a)",
+        b : "torch.tensor(a)",
+        c: 'a.tensor()',
+        d: 'a.torch.tensor()',
+        correct: 'b',
+    },
+    {
+        quiz : "Q1.Numpy : Create an array of shape (3, 2) and populate it with random samples from a uniform distribution over [0, 1).",
+        a : "array.randome(3,2)",
+        b : "np.array(3,2)",
+        c : "np.array.rand(3,4)",
+        d: "np.array(rand(3,4))",
+        correct : 'c'
     },
     {
         quiz : "inside which HTML element do we put the Javascript",
@@ -24,21 +32,48 @@ const quizData=[
         correct : 'd'
     },
 ]
+const quizContainer =   document.querySelector('.quiz-container')
 const quiz = document.getElementById('quiz-text')
-console.log('quiz', quiz);
 const a = document.getElementById('answer_a')
 const b = document.getElementById('answer_b')
 const c = document.getElementById('answer_c')
 const d = document.getElementById('answer_d')
-
+const btn = document.getElementById('btnSubmit');
 let quizIndex = 0;
+let currentQuiz = {};
+let score = 0;
 loadQuiz()
 function loadQuiz(){
-    let currentQuiz = quizData[quizIndex];
-    console.log('currentQuiz', currentQuiz.quiz);
+    currentQuiz = quizData[quizIndex];
     quiz.innerText=currentQuiz.quiz;
     a.innerText=currentQuiz.a;
     b.innerText=currentQuiz.b;
     c.innerText=currentQuiz.c;
     d.innerText=currentQuiz.d;
-}a
+}
+function getSelectedAnswer(){
+    const answerEl =   document.querySelectorAll('.answer');
+    let submittedAnswer;
+    answerEl.forEach(el =>{
+        if (el.checked) {
+            submittedAnswer = el.id;
+        }
+    })
+    return submittedAnswer;
+}
+const onSubmitQuiz = (()=>{
+    let selectedAnswer = getSelectedAnswer();
+    if(selectedAnswer === currentQuiz.correct){
+        score++;
+    }
+    if(quizIndex < quizData.length-1)
+        loadQuiz(quizIndex++);
+    else{
+        quizContainer.innerHTML = `
+        <h2>You answered correctly at ${score}/${quizData.length} questions.</h2>
+        
+        <button onclick="location.reload()">Reload</button>
+    `;
+    }
+})
+btn.addEventListener('click', onSubmitQuiz);
